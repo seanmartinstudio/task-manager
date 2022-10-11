72747d6849b75f55aec6b5902e1566e471114f33
Sean Martin <sean@seanmartinstudio.com>
2022-10-11 14:41:40 -0700

    adds Signup and Login buttons

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const LoginForm = () => {
   return (
-    <h2>Login Form</h2>
+    <form>
+        <h2>LoginForm Component</h2>
+        {/* <label for="username">Username</label> */}
+        <input type="text" id="username" name="username" placeholder="Username"></input>
+
+        {/* <label for="password">Password</label> */}
+        <input type="text" id="password" name="password" placeholder="Pasword"></input>
+
+        <br></br>
+        <button class="button">Login</button>
+
+        <br></br>
+        <button class="button">New User Signup</button>
+
+
+    </form>
   )
 }
+
 
 export default LoginForm
