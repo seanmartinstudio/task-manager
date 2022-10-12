0efcb9902f9280057834b3d914ea479645efcba6
Sean Martin <sean@seanmartinstudio.com>
2022-10-12 15:22:22 -0700

    adds Route Links between Login Page and Signup Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import LoginForm from '../components/LoginForm'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
   return (
-    <div>
-    <h1>Login Page</h1>
-    <LoginForm/>
-    </div>
+    <form>
+    <h2>Login Page</h2>
+    {/* <label for="username">Username</label> */}
+    <input type="text" id="username" name="username" placeholder="Username"></input>
 
-  )
+    {/* <label for="password">Password</label> */}
+    <input type="text" id="password" name="password" placeholder="Pasword"></input>
+
+    <br></br>
+    <button class="button">Login</button>
+
+    <br></br>
+    <Link to="/signup">
+    <button class="button">New User? Sign Up Here</button>
+    </Link>
+
+  </form>
+)
 }
-
-
 
 export default LoginPage
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <form>
+    <h2>Signup Page</h2>
+ 
+    <input type="text" id="username" name="username" placeholder="Username"></input>
+
+    <input type="text" id="password" name="password" placeholder="Password"></input>
+
+    <input type="text" id="password confirmation" name="passwordConfirmation" placeholder="Password Confirmation"></input>
+
+    <input type="text" id="jobTitle" name="jobTitle" placeholder="Job Title"></input>
+
+    <br></br>
+    <button class="button">Sign Up</button>
+
+    <br></br>
+    <Link to="/">
+    <button class="button">Already a user? Login Here</button>
+    </Link>
+
+  </form>
   )
 }
 
