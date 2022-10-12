46db66f2e63e248f05a7a3751b33e1d33d908de8
Sean Martin <sean@seanmartinstudio.com>
2022-10-12 15:48:29 -0700

    adds submit logic to Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import { useState } from "react";
 import { Link } from 'react-router-dom'
 
-const LoginPage = () => {
-  return (
-    <form>
-    <h2>Login Page</h2>
-    {/* <label for="username">Username</label> */}
-    <input type="text" id="username" name="username" placeholder="Username"></input>
 
-    {/* <label for="password">Password</label> */}
-    <input type="text" id="password" name="password" placeholder="Pasword"></input>
+const LoginPage = () => {
+
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+    console.log(username)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+    console.log(password)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    console.log(username, password)
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+    <h2>Login Page</h2>
+  
+    <input type="text" id="username" name="username" placeholder="Username" onChange={handleUsernameChange} value={username}></input>
+
+    <input type="text" id="password" name="password" placeholder="Password" onChange={handlePasswordChange} value={password}></input>
 
     <br></br>
     <button class="button">Login</button>
@@ -18,7 +38,6 @@ const LoginPage = () => {
     <Link to="/signup">
     <button class="button">New User? Sign Up Here</button>
     </Link>
-
   </form>
 )
 }
