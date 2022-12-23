8b5eebc1531931ff8f3f79473b8290ad66ce8014
Sean Martin <sean@seanmartinstudio.com>
2022-12-23 14:13:12 -0800

    adds additional authentication to TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,16 @@ class TasksController < ApplicationController
     #GET '/tasks'
     def index 
         user = User.find_by(id: session[:user_id])
-        userid = user.id
-        tasks = Task.where(user_id: userid)
-        if tasks
-            render json: tasks
+            if user
+            user_id = user.id
+            tasks = Task.where(user_id: user_id)
+            if tasks
+                render json: tasks
+            else
+                render json: []
+            end
         else
-            render json: []
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
         end
     end
 
@@ -18,13 +22,17 @@ class TasksController < ApplicationController
     #PATCH '/tasks/:id'
     def update
         user = User.find_by(id: session[:user_id])
-        user_id = user.id
-        task = Task.find_by(id: params[:id])
-        if task.user_id == user_id
-            task.update(update_task_params)
-            render json: task
+        if user
+            user_id = user.id
+            task = Task.find_by(id: params[:id])
+            if task.user_id == user_id
+                task.update(update_task_params)
+                render json: task
+            else
+                render json: { error: ["Not Found"] }, status: :not_found
+            end
         else
-            render json: { error: "Task Not Found" }, status: :not_found
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
         end
     end
 
@@ -32,13 +40,17 @@ class TasksController < ApplicationController
     #DELETE '/tasks/:id'
     def destroy 
         user = User.find_by(id: session[:user_id])
-        user_id = user.id
-        task = Task.find_by(id: params[:id])
-        if task 
-            task.destroy
-            head :no_content
+        if user
+            user_id = user.id
+            task = Task.find_by(id: params[:id])
+            if task.user_id == user_id 
+                task.destroy
+                head :no_content
+            else
+                render json: { error: ["Not Found"] }, status: :not_found
+            end
         else
-            render json: { error: "Task not found" }, status: :not_found
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
         end
     end
 
@@ -54,8 +66,32 @@ class TasksController < ApplicationController
         end
     end
 
-    private
+
     
+
+        ### CODE PRACTICE
+        def search_category
+            category = Category.find_by(category_title: params[:category_title])
+            tasks = category.tasks
+            render json: tasks
+        end
+    
+        ## CODE PRACTICE
+        ## TASK: "Create a route that lets us search for a task that has a certain number of words"
+        def task_number_2 
+            #Grab the value of the parameter and convert to int
+            parameter = params[:integer]
+            parameter = parameter.to_i
+            #Line up all tasks
+            tasks = Task.all
+            #Iterate through each task, as the question "Does the body have 'params' amount of words in the string?"
+            correct_task = tasks.find { |task| task.body.split.count == parameter }
+            #Return obj as JSON
+            render json: correct_task
+        end
+
+    private
+
     def update_task_params
         params.permit(:complete)
     end
